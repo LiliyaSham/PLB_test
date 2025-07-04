0d2d428cd744d1155de016adf5a9b7c0a5dbe06d
LiliyaShammasova <leelia@mail.ru>
2025-07-04 20:17:51 +0300

    task1

---
+++ task1/task1.py
@@ -0,0 +1,17 @@
+n, m = map(int, input().split())
+
+path = []
+start_ind = 0
+visited_start_ind = set()
+
+while True:
+    value = start_ind + 1
+    if value in visited_start_ind:
+        break
+    path.append(value)
+    visited_start_ind.add(value)
+    start_ind = (start_ind + m - 1) % n
+    if start_ind == 0:  # если вернулись к первому элементу
+        break
+
+print(*path)
